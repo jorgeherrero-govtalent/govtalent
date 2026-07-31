aebb7912a221aea8a14fd13fd1dc5a62289d1e20
jorgeherrero-govtalent <jorgerafaelherrerovidal@gmail.com>
2026-07-31 14:39:46 +0200

    Replace mailto link with Next.js Link component

--- components/UpgradeModal.js
+++ components/UpgradeModal.js
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function UpgradeModal({ title, message, onClose }) {
   return (
     <div className="modal-ov on" onClick={(e) => e.target === e.currentTarget && onClose()}>
@@ -13,13 +15,14 @@ export default function UpgradeModal({ title, message, onClose }) {
           </div>
         </div>
         <p style={{ fontSize: 13, color: '#666', margin: '4px 0 22px', lineHeight: 1.6 }}>{message}</p>
-        <a
-          href="mailto:hola@govtalent.app"
+        <Link
+          href="/precios"
+          target="_blank"
           className="btn-p"
           style={{ textDecoration: 'none', display: 'inline-flex', alignItems: 'center', gap: 6 }}
         >
-          <i className="ti ti-mail"></i> Hablar sobre el upgrade
-        </a>
+          <i className="ti ti-arrow-up-right"></i> Ver planes
+        </Link>
       </div>
     </div>
   );
